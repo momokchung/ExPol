f0736f29021fea70dc73fd0a7e3b673733804fb8
momokchung <kchung25@wustl.edu>
2022-04-13 17:40:11 -0500

    Added spring parameter to expol

--- source/expol.f
+++ source/expol.f
@@ -13,6 +13,7 @@ c     ##################################################################
 c
 c
 c     nexpol    total number of exch polarization sites in the system
+c     kpep      exchange polarization spring constant at each site
 c     prepep    exchange polarization constant prefactor at each site
 c     dmppep    exchange polarization damping alpha at each site
 c     scrtyp    type of screening
@@ -21,6 +22,7 @@ c
       module expol
       implicit none
       integer nexpol
+      real*8, allocatable :: kpep(:)
       real*8, allocatable :: prepep(:)
       real*8, allocatable :: dmppep(:)
       character*3 scrtyp
